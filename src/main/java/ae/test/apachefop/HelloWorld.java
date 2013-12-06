34548aacd3c00dfd79231b644cbaabfbe951874e
p0gram3r <p0gram3r@gmail.com>
2013-12-06 13:13:58 +0100

    creating initial project structure

---
+++ src/main/java/ae/test/apachefop/HelloWorld.java
@@ -0,0 +1,13 @@
+package codingdojo;
+
+public class HelloWorld {
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().getGreeting());
+    }
+
+}
