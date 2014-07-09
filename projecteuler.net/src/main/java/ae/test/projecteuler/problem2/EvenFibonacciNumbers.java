b98c07282890fc1b1cec35480706645f2f0a9d70
André Ehrlich <anehrlich@paypal.com>
2014-07-09 14:16:05 +0200

    solving problem 2

---
+++ projecteuler.net/src/main/java/ae/test/projecteuler/problem2/EvenFibonacciNumbers.java
@@ -0,0 +1,15 @@
+package ae.test.projecteuler.problem2;
+
+public class EvenFibonacciNumbers
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
