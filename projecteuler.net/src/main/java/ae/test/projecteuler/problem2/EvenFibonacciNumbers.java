2da48727e14df775242cc665fa86969f4a35d3de
André Ehrlich <anehrlich@paypal.com>
2014-07-09 14:16:27 +0200

    solving problem 2

--- projecteuler.net/src/main/java/ae/test/projecteuler/problem2/EvenFibonacciNumbers.java
+++ projecteuler.net/src/main/java/ae/test/projecteuler/problem2/EvenFibonacciNumbers.java
@@ -2,14 +2,27 @@ package ae.test.projecteuler.problem2;
 
 public class EvenFibonacciNumbers
 {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		int limit = 4000000;
+		long sum = 0;
 
+		// F0 and F1
+		long a = 0;
+		long b = 1;
+
+		while (b < limit)
+		{
+			long temp = b;
+			b = a + b;
+			a = temp;
+
+			if (b % 2 == 0)
+			{
+				sum += b;
+			}
+		}
+
+		System.out.println(sum);
 	}
-
 }
