557493e81357ae02a4bbcabc072702de8a27291a
André Ehrlich <anehrlich@paypal.com>
2014-07-11 16:48:23 +0200

    solving problem 7

---
+++ projecteuler.net/src/main/java/problem7/PrimeNumber10001.java
@@ -0,0 +1,24 @@
+package problem7;
+
+import utils.IntegerUtils;
+
+public class PrimeNumber10001
+{
+	public static void main(String[] args)
+	{
+		int currentNumber = 0;
+		int index = 0;
+		do
+		{
+			currentNumber++;
+
+			if (IntegerUtils.isPrim(currentNumber))
+			{
+				index++;
+				System.out.println("index " + index + ": " + currentNumber);
+			}
+
+		}
+		while (index < 10001);
+	}
+}
