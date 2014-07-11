ec66ac7aad0269030c23bfbfafc8291138b050f0
André Ehrlich <anehrlich@paypal.com>
2014-07-11 16:55:31 +0200

    solving problem 6

---
+++ projecteuler.net/src/main/java/problem6/SumSquareDifference.java
@@ -0,0 +1,22 @@
+package problem6;
+
+public class SumSquareDifference
+{
+	public static void main(String[] args)
+	{
+		int N = 100;
+
+		long sum1 = 0;
+		long sum2 = 0;
+		for (int i = 1; i <= N; i++)
+		{
+			sum1 += i * i;
+			sum2 += i;
+		}
+
+		sum2 = sum2 * sum2;
+
+		System.out.println(sum2 - sum1);
+
+	}
+}
