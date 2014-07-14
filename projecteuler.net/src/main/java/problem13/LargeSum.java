ea80dbf3f25a7e8bef51b38552bea33c9c6f41e8
André Ehrlich <anehrlich@paypal.com>
2014-07-14 15:19:58 +0200

    solving problem 13

---
+++ projecteuler.net/src/main/java/problem13/LargeSum.java
@@ -0,0 +1,29 @@
+package problem13;
+
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class LargeSum
+{
+	public static void main(String[] args)
+	{
+		BigInteger sum = BigInteger.ZERO;
+
+		InputStream inStream = LargeSum.class.getClassLoader().getResourceAsStream("problem13.txt");
+		Scanner scanner = new Scanner(inStream);
+
+		while (scanner.hasNext())
+		{
+			String number = scanner.nextLine();
+			sum = sum.add(new BigInteger(number));
+		}
+
+		System.out.println(sum);
+
+		String s = sum.toString();
+		System.out.println(s.length());
+		System.out.println(s.substring(0, 10));
+	}
+
+}
