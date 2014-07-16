5ee8cf25721253a3ed4e995e54c1b231b9662391
André Ehrlich <anehrlich@paypal.com>
2014-07-16 16:02:31 +0200

    refactoring: moving useful helper methods to LongUtils

--- projecteuler.net/src/main/java/probelm12/HighlyDivisibleTriangularNumber.java
+++ projecteuler.net/src/main/java/probelm12/HighlyDivisibleTriangularNumber.java
@@ -1,35 +1,27 @@
 package probelm12;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.TreeSet;
+
+import utils.LongUtils;
 
 public class HighlyDivisibleTriangularNumber
 {
 	public static void main(String[] args)
 	{
 		int i = 1;
+		long triangleNumber = 0;
 		int divisorCount = 0;
 
 		do
 		{
-			long number = calculateTriangleNumber(i);
-
-			Collection<Long> allDivisors = getAllDivisors(number);
-
+			triangleNumber = calculateTriangleNumber(i);
+			Collection<Long> allDivisors = LongUtils.getAllDivisors(triangleNumber);
 			divisorCount = allDivisors.size();
-			System.out.println(i + " -> " + number + ": " + divisorCount);
-
 			i = i + 1;
 		}
 		while (divisorCount < 500);
 
-		// long number = 54197677995L;
-		// Collection<Long> allDivisors = getAllDivisors(number);
-		// System.out.println(allDivisors);
-		// System.out.println(allDivisors.size());
+		System.out.println(i + " -> " + triangleNumber + ": " + divisorCount);
 	}
 
 
@@ -39,83 +31,4 @@ public class HighlyDivisibleTriangularNumber
 		return n * (n + 1) / 2;
 	}
 
-
-
-	private static int countDivisiors(long number)
-	{
-		// all numbers can be divided by themselves
-		int count = 1;
-
-		Collection<Long> divs = new LinkedList<Long>();
-
-		for (long i = 1; i * 2 <= number; i++)
-		{
-			if (number % i == 0)
-			{
-				count++;
-				divs.add(i);
-			}
-		}
-		System.out.println(divs);
-
-		return count;
-	}
-
-
-
-	private static List<Long> getPrimFactors(long number)
-	{
-		List<Long> factors = new ArrayList<Long>();
-		for (long i = 2; i * i <= number; i++)
-		{
-			while (number % i == 0)
-			{
-				factors.add(i);
-				number /= i;
-			}
-		}
-		if (number > 1)
-		{
-			factors.add(number);
-		}
-		return factors;
-	}
-
-
-
-	private static Collection<Long> getAllDivisors(long number)
-	{
-		List<Long> primFactors = getPrimFactors(number);
-
-		Collection<Long> allDivisors = new TreeSet<Long>();
-		allDivisors.add(1L);
-		allDivisors.addAll(multiples(primFactors));
-
-		return allDivisors;
-	}
-
-
-
-	private static List<Long> multiples(List<Long> $list)
-	{
-		if ($list.size() <= 1)
-		{
-			return $list;
-		}
-
-		long $x = $list.remove(0);
-
-		List<Long> $res = new LinkedList<Long>();
-		$res.add($x);
-
-		List<Long> $sub = multiples($list);
-
-		for (long $item : $sub)
-		{
-			$res.add($item);
-			$res.add($x * $item);
-		}
-
-		return $res;
-	}
 }
--- projecteuler.net/src/main/java/utils/IntegerUtils.java
+++ projecteuler.net/src/main/java/utils/IntegerUtils.java
@@ -9,30 +9,7 @@ public class IntegerUtils
 
 	public static boolean isPrim(int number)
 	{
-		if (number < 2)
-		{
-			return false;
-		}
-
-		if (number == 2)
-		{
-			return true;
-		}
-
-		if (number % 2 == 0)
-		{
-			return false;
-		}
-
-		for (int i = 3; i * i <= number; i = i + 2)
-		{
-			if (number % i == 0)
-			{
-				return false;
-			}
-		}
-
-		return true;
+		return LongUtils.isPrim(number);
 	}
 
 
