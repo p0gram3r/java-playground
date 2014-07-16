6096e56c9b510a24b0508c757c239807541104f4
André Ehrlich <anehrlich@paypal.com>
2014-07-16 16:58:51 +0200

    solving problem 16

---
+++ projecteuler.net/src/main/java/problem16/PowerDigitSum.java
@@ -0,0 +1,34 @@
+package problem16;
+
+import java.math.BigInteger;
+
+public class PowerDigitSum
+{
+
+	public static void main(String[] args)
+	{
+		BigInteger base = new BigInteger("2");
+		int exponent = 1000;
+
+		BigInteger result = base.pow(exponent);
+		long sumOfDigits = sumDigits(result);
+
+		System.out.println(sumOfDigits);
+	}
+
+
+
+	private static long sumDigits(BigInteger number)
+	{
+		long sumOfDigits = 0;
+
+		String s = number.toString();
+		for (int i = 0; i < s.length(); i++)
+		{
+			char c = s.charAt(s.length() - i - 1);
+			sumOfDigits += Long.valueOf(new StringBuilder().append(c).toString());
+		}
+
+		return sumOfDigits;
+	}
+}
