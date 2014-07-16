0de53264200fe9586e581b33b25426842de40845
André Ehrlich <anehrlich@paypal.com>
2014-07-16 17:09:16 +0200

    solving problem 48

---
+++ projecteuler.net/src/main/java/package48/SelfPowers.java
@@ -0,0 +1,26 @@
+package package48;
+
+import java.math.BigInteger;
+
+public class SelfPowers
+{
+
+	public static void main(String[] args)
+	{
+		BigInteger result = BigInteger.ZERO;
+
+		for (int i = 1; i <= 1000; i++)
+		{
+			BigInteger base = new BigInteger(Integer.toString(i));
+
+			result = result.add(base.pow(i));
+		}
+
+		String resultString = result.toString();
+		// System.out.println(resultString);
+
+		int len = resultString.length();
+		System.out.println(resultString.substring(len - 10));
+	}
+
+}
