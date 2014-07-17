a55b12e2213e4f043b159e5d59180f4d5ebe2120
André Ehrlich <anehrlich@paypal.com>
2014-07-17 10:47:32 +0200

    solving problem 52

---
+++ projecteuler.net/src/main/java/problem52/PermutedMultiples.java
@@ -0,0 +1,35 @@
+package problem52;
+
+import java.util.Collection;
+
+import utils.IntegerUtils;
+
+public class PermutedMultiples
+{
+	public static void main(String[] args)
+	{
+		int x = 1;
+		while (true)
+		{
+			Collection<Integer> permutations = IntegerUtils.getPermutations(x);
+
+			boolean containsAllMultiples = true;
+			containsAllMultiples &= permutations.contains(x);
+			containsAllMultiples &= permutations.contains(2 * x);
+			containsAllMultiples &= permutations.contains(3 * x);
+			containsAllMultiples &= permutations.contains(4 * x);
+			containsAllMultiples &= permutations.contains(5 * x);
+			containsAllMultiples &= permutations.contains(6 * x);
+
+			if (containsAllMultiples)
+			{
+				break;
+			}
+
+			x++;
+		}
+
+		System.out.println(x);
+	}
+
+}
