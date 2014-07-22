11782ea850c8ebff2920021caf3f0ffbd4b8a213
André Ehrlich <anehrlich@paypal.com>
2014-07-22 18:16:09 +0200

    WIP problem 79

--- projecteuler.net/src/main/java/problem79/PasscodeDerivation.java
+++ projecteuler.net/src/main/java/problem79/PasscodeDerivation.java
@@ -1,62 +1,67 @@
 package problem79;
 
 import java.io.InputStream;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class PasscodeDerivation
 {
 
-    public static void main(String[] args)
-    {
-        List<String> keylog = parseKeylog();
+	public static void main(String[] args)
+	{
+		List<String> keylog = parseKeylog();
 
-        Map<Character, Integer> occIndex0 = getOccurences(keylog, 0);
-        Map<Character, Integer> occIndex1 = getOccurences(keylog, 1);
-        Map<Character, Integer> occIndex2 = getOccurences(keylog, 2);
+		Set<Character> occIndex0 = getOccurences(keylog, 0);
+		Set<Character> occIndex1 = getOccurences(keylog, 1);
+		Set<Character> occIndex2 = getOccurences(keylog, 2);
 
-        System.out.println(occIndex0);
-        System.out.println(occIndex1);
-        System.out.println(occIndex2);
-    }
+		Collection<Character> usedCharacters = new HashSet<Character>();
+		usedCharacters.addAll(occIndex0);
+		usedCharacters.addAll(occIndex1);
+		usedCharacters.addAll(occIndex2);
+	
+		Set<Character> firstChars = new HashSet<Character>();
+		for(char c : usedCharacters)
+		{
+			if (oc)
+		}
 
-
-
-    private static List<String> parseKeylog()
-    {
-        InputStream inStream = PasscodeDerivation.class.getClassLoader().getResourceAsStream("problem79.txt");
-
-        List<String> result = new LinkedList<String>();
-
-        Scanner scanner = new Scanner(inStream);
-        while (scanner.hasNext())
-        {
-            result.add(scanner.next());
-        }
-
-        return result;
-    }
+	}
 
 
 
-    private static Map<Character, Integer> getOccurences(List<String> keylog, int index)
-    {
-        Map<Character, Integer> result = new HashMap<Character, Integer>();
+	private static List<String> parseKeylog()
+	{
+		InputStream inStream = PasscodeDerivation.class.getClassLoader().getResourceAsStream("problem79.txt");
 
-        for (String s : keylog)
-        {
-            char c = s.charAt(index);
-            Integer i = result.get(c);
-            if (i == null)
-            {
-                i = 0;
-            }
-            result.put(c, i + 1);
-        }
+		List<String> result = new LinkedList<String>();
 
-        return result;
-    }
+		Scanner scanner = new Scanner(inStream);
+		while (scanner.hasNext())
+		{
+			result.add(scanner.next());
+		}
+
+		return result;
+	}
+
+
+
+	private static Set<Character> getOccurences(List<String> keylog, int index)
+	{
+		Set<Character> result = new HashSet<Character>();
+
+		for (String s : keylog)
+		{
+			char c = s.charAt(index);
+			result.add(c);
+		}
+
+		return result;
+	}
 }
