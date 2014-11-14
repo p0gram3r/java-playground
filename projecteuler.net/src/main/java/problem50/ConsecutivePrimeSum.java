714ccb4a95fb7751d7c6014b911d65aad4efc94c
anehrlich <anehrlich@paypal.com>
2014-11-14 16:21:13 +0100

    solution for 100

--- projecteuler.net/src/main/java/problem50/ConsecutivePrimeSum.java
+++ projecteuler.net/src/main/java/problem50/ConsecutivePrimeSum.java
@@ -7,6 +7,7 @@ import utils.IntegerUtils;
 
 public class ConsecutivePrimeSum
 {
+	// 41 = 2 + 3 + 5 + 7 + 11 + 13
 	public static void main(String[] args)
 	{
 		int limit = 100;
@@ -14,10 +15,43 @@ public class ConsecutivePrimeSum
 		// create a list of all primes up to limit
 		List<Integer> primes = createListOfPrimesUpTo(limit);
 
-		// 41 = 2 + 3 + 5 + 7 + 11 + 13
-
 		System.out.println(primes);
 
+		List<Integer> asdf = new LinkedList<Integer>();
+
+		for (int i = 0; i < primes.size(); i += 1)
+		{
+			int sum = primes.get(i);
+
+			List<Integer> temp = new LinkedList<Integer>();
+			temp.add(primes.get(i));
+
+			for (int j = i + 1; j < primes.size(); j += 1)
+			{
+				sum += primes.get(j);
+				temp.add(primes.get(j));
+				if (primes.contains(sum))
+				{
+					System.out.println("   sum: " + sum + " --- " + temp);
+
+					if (asdf.size() < temp.size())
+					{
+						asdf = new LinkedList<Integer>(temp);
+					}
+				}
+
+			}
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println(asdf);
+
+		int sum = 0;
+		for (int i : asdf)
+		{
+			sum += i;
+		}
+		System.out.println(" ==> " + sum);
 	}
 
 
