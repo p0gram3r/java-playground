c4fd2d772a3ae3e140e34f97b52e8bf8cf5a0c07
anehrlich <anehrlich@paypal.com>
2014-11-17 16:36:51 +0100

    initial version

---
+++ projecteuler.net/src/main/java/problem201/SubsetsWithUniqueSum.java
@@ -0,0 +1,15 @@
+package problem201;
+
+public class SubsetsWithUniqueSum
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
