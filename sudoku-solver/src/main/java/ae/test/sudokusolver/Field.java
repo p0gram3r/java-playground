ce7e0f1640032fe764a0ba0ec3f3bd77e8223a46
p0gram3r <p0gram3r@gmail.com>
2014-11-24 19:06:47 +0100

    no possibilities left after value is set

--- sudoku-solver/src/main/java/ae/test/sudokusolver/Field.java
+++ sudoku-solver/src/main/java/ae/test/sudokusolver/Field.java
@@ -5,40 +5,38 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class Field {
-    private Integer value = null;
-    private SortedSet<Integer> possibleValues = new TreeSet<Integer>();
+	private Integer value = null;
+	private SortedSet<Integer> possibleValues = new TreeSet<Integer>();
 
-    public Field() {
-        for (int i = 1; i <= 9; i += 1) {
-            possibleValues.add(i);
-        }
-    }
+	public Field() {
+		for (int i = 1; i <= 9; i += 1) {
+			possibleValues.add(i);
+		}
+	}
 
-    public Field(int value) {
-        setValue(value);
-    }
+	public Field(int value) {
+		setValue(value);
+	}
 
-    public Integer getValue() {
-        return value;
-    }
+	public Integer getValue() {
+		return value;
+	}
 
-    public void setValue(int value) {
-        this.value = value;
+	public void setValue(int value) {
+		this.value = value;
+		possibleValues.clear();
+	}
 
-        possibleValues.clear();
-        possibleValues.add(value);
-    }
+	public SortedSet<Integer> getPossibleValues() {
+		return Collections.unmodifiableSortedSet(possibleValues);
+	}
 
-    public SortedSet<Integer> getPossibleValues() {
-        return Collections.unmodifiableSortedSet(possibleValues);
-    }
+	public void removePossibility(int possibility) {
+		possibleValues.remove(possibility);
+	}
 
-    public void removePossibility(int possibility) {
-        possibleValues.remove(possibility);
-    }
-
-    @Override
-    public String toString() {
-        return (value == null ? "_" : String.valueOf(value));
-    }
+	@Override
+	public String toString() {
+		return (value == null ? "_" : String.valueOf(value));
+	}
 }
--- sudoku-solver/src/test/java/ae/test/sudokusolver/FieldTest.java
+++ sudoku-solver/src/test/java/ae/test/sudokusolver/FieldTest.java
@@ -2,7 +2,7 @@ package ae.test.sudokusolver;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -10,34 +10,35 @@ import org.junit.Test;
 
 public class FieldTest {
 
-    @Test
-    public void testGetPossibleValuesAfterConstruction() {
-        Field field = new Field();
+	@Test
+	public void testGetPossibleValuesAfterConstruction() {
+		Field field = new Field();
 
-        assertThat(field.getValue(), is(nullValue()));
-        assertThat(field.getPossibleValues(), contains(1, 2, 3, 4, 5, 6, 7, 8, 9));
-    }
+		assertThat(field.getValue(), is(nullValue()));
+		assertThat(field.getPossibleValues(),
+				contains(1, 2, 3, 4, 5, 6, 7, 8, 9));
+	}
 
-    @Test
-    public void testGetPossibleValuesAfterSettingValue() {
-        Field field = new Field();
-        field.setValue(5);
+	@Test
+	public void testGetPossibleValuesAfterSettingValue() {
+		Field field = new Field();
+		field.setValue(5);
 
-        assertThat(field.getValue(), is(5));
-        assertThat(field.getPossibleValues(), hasSize(1));
-        assertThat(field.getPossibleValues(), contains(5));
-    }
+		assertThat(field.getValue(), is(5));
+		assertThat(field.getPossibleValues(), is(empty()));
+	}
 
-    @Test
-    public void testRemovePossibility() {
-        Field field = new Field();
+	@Test
+	public void testRemovePossibility() {
+		Field field = new Field();
 
-        assertThat(field.getPossibleValues(), contains(1, 2, 3, 4, 5, 6, 7, 8, 9));
+		assertThat(field.getPossibleValues(),
+				contains(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-        field.removePossibility(5);
-        assertThat(field.getPossibleValues(), contains(1, 2, 3, 4, 6, 7, 8, 9));
+		field.removePossibility(5);
+		assertThat(field.getPossibleValues(), contains(1, 2, 3, 4, 6, 7, 8, 9));
 
-        field.removePossibility(5);
-        assertThat(field.getPossibleValues(), contains(1, 2, 3, 4, 6, 7, 8, 9));
-    }
+		field.removePossibility(5);
+		assertThat(field.getPossibleValues(), contains(1, 2, 3, 4, 6, 7, 8, 9));
+	}
 }
