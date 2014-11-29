45eb5f0cd1face6974ee0246bbc045f8bde47cbb
p0gram3r <p0gram3r@gmail.com>
2014-11-29 16:16:53 +0100

    initial version

---
+++ sudoku-solver/src/main/java/ae/test/sudokusolver/solver/EliminationSolver.java
@@ -0,0 +1,26 @@
+package ae.test.sudokusolver.solver;
+
+import ae.test.sudokusolver.Sudoku;
+
+/**
+ * Solver based on elimination algorithm created by Robert Kosten and p0gram3r.
+ */
+public class EliminationSolver {
+
+    public void solve(Sudoku sudoku) {
+
+        while (!isSolved(sudoku)) {
+
+        }
+    }
+
+    public boolean isSolved(Sudoku sudoku) {
+        int dimension = sudoku.getDimension();
+        for (int i = 0; i < 9; i += 1) {
+
+        }
+
+        return true;
+    }
+
+}
