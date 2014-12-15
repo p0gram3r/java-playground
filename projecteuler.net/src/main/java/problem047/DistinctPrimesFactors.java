813bfde5d540231d0983cf291047983a3142f39b
anehrlich <anehrlich@paypal.com>
2014-12-15 16:50:57 +0100

    solving problem 47

---
+++ projecteuler.net/src/main/java/problem047/DistinctPrimesFactors.java
@@ -0,0 +1,32 @@
+package problem047;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import utils.LongUtils;
+
+public class DistinctPrimesFactors {
+
+    public static void main(String[] args) {
+        int LIMIT = 4;
+
+        long result = 2;
+        int counter = 0;
+        for (long currentNumber = 2; counter < LIMIT; currentNumber += 1) {
+
+            if (uniquePrimeFactors(currentNumber).size() == LIMIT) {
+                result = currentNumber - counter;
+                counter += 1;
+            }
+            else {
+                counter = 0;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static Set<Long> uniquePrimeFactors(long number) {
+        return new TreeSet<Long>(LongUtils.getPrimFactors(number));
+    }
+}
