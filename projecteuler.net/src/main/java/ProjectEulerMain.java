acb39ef08c4044093475d4db8d04a053d96133aa
anehrlich <anehrlich@paypal.com>
2014-12-15 17:23:05 +0100

    moving first problem

--- projecteuler.net/src/main/java/ProjectEulerMain.java
+++ projecteuler.net/src/main/java/ProjectEulerMain.java
@@ -1,9 +1,10 @@
+import problem.EvenFibonacciNumbers;
 import problem.Problem;
 
 public class ProjectEulerMain {
 
     public static void main(String[] args) {
-        Problem p = new Problem002();
+        Problem p = new EvenFibonacciNumbers();
 
         System.out.println(p.getSolution());
     }
