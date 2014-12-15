e37e4ed3b80f9b21dda444a50f7335f05fb63122
anehrlich <anehrlich@paypal.com>
2014-12-15 17:20:20 +0100

    preparing refactoring of structure

---
+++ projecteuler.net/src/main/java/ProjectEulerMain.java
@@ -0,0 +1,10 @@
+import problem.Problem;
+
+public class ProjectEulerMain {
+
+    public static void main(String[] args) {
+        Problem p = new Problem002();
+
+        System.out.println(p.getSolution());
+    }
+}
