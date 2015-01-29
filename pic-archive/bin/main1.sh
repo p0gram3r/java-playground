91e275ae4156ef5604036e0de9836a82564424aa
anehrlich <anehrlich@paypal.com>
2015-01-29 10:50:51 +0100

    adding second Main class

---
+++ pic-archive/bin/main1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)/..
+
+java -cp $BASEDIR/target/*.jar ae/picarchive/Main1
---
+++ pic-archive/src/main/java/ae/picarchive/Main2.java
@@ -0,0 +1,9 @@
+package ae.picarchive;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        System.out.println("main2");
+
+    }
+}
