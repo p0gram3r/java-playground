935962e0138226e15e781edcba869e7e59001de4
p0gram3r <p0gram3r@gmail.com>
2015-01-29 01:28:04 +0100

    removing file_id from urls table

--- pic-archive/src/main/resources/db/1.sql
+++ pic-archive/src/main/resources/db/1.sql
@@ -1,11 +1,11 @@
-create table files (
-	id INTEGER IDENTITY,
-	path VARCHAR(500) NOT NULL UNIQUE
-);
-
 create table urls (
 	id INTEGER IDENTITY,
 	url varchar(500) NOT NULL UNIQUE,
-	status varchar(10) DEFAULT 'NEW' NOT NULL check (status in ('NEW', 'ERROR', 'DONE')),
-	file_id INTEGER REFERENCES files(id)
+	status varchar(10) DEFAULT 'NEW' NOT NULL check (status in ('NEW', 'WIP', 'ERROR', 'DONE'))
 );
+
+-- TODO
+-create table files (
+-    name varchar(35) PRIMARY KEY,
+-    path VARCHAR(500) NOT NULL
+-);
