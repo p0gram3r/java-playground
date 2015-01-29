d5270fd24df16077e085d1a9ea7a66fffa632170
p0gram3r <p0gram3r@gmail.com>
2015-01-29 23:57:24 +0100

    write urls to separate file before downloading them

--- pic-archive/bin/exportDownloads.sh
+++ pic-archive/bin/exportDownloads.sh
@@ -2,4 +2,16 @@
 
 BASEDIR=$(dirname $0)/..
 
-java -cp $BASEDIR/target/pic-archive.jar ae/picarchive/ExportWipUrlsAsWgetDownload
+TMP_FILE=$BASEDIR/bin/$(date +%s-%N)
+
+echo "exporting urls to temp file $TMP_FILE"
+java -cp $BASEDIR/target/pic-archive.jar ae/picarchive/ExportWipUrlsAsWgetDownload >> $TMP_FILE
+
+echo "changing file permissions"
+chmod ugo+x $TMP_FILE
+
+echo "executing file"
+$TMP_FILE
+
+echo "removing temp file"
+rm $TMP_FILE
