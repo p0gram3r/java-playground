a5e78ab97830402fde9b1670c8639b9515530aeb
p0gram3r <p0gram3r@gmail.com>
2015-01-29 23:10:15 +0100

    adding command to prepare downloads

---
+++ pic-archive/src/main/java/ae/picarchive/PrepareDownloads.java
@@ -0,0 +1,27 @@
+package ae.picarchive;
+
+import static ae.picarchive.Configuration.DB_PASS;
+import static ae.picarchive.Configuration.DB_URL;
+import static ae.picarchive.Configuration.DB_USER;
+import ae.picarchive.dao.DAOFactory;
+import ae.picarchive.dao.UrlDAO;
+
+public class PrepareDownloads {
+
+    public static void main(String[] args) {
+        DAOFactory daoFactory = new DAOFactory(DB_URL, DB_USER, DB_PASS);
+        UrlDAO dao = daoFactory.getUrlDAO();
+
+        int limit = 10;
+        if (args.length == 1) {
+            try {
+                limit = Integer.parseInt(args[0]);
+            }
+            catch (NumberFormatException e) {
+                // use default
+            }
+        }
+
+        dao.markUrlsAsWip(limit);
+    }
+}
