409c6b9c71996a9e97a52e417ec57fac8957e7a7
anehrlich <anehrlich@paypal.com>
2015-01-29 18:21:05 +0100

    using wget format for export

--- pic-archive/src/main/java/ae/picarchive/ExportDownloadList.java
+++ pic-archive/src/main/java/ae/picarchive/ExportDownloadList.java
@@ -9,15 +9,17 @@ import ae.picarchive.entity.Url;
 
 public class ExportDownloadList {
 
+    private static final String DOWNLOAD_DIR = "_downloads";
+
     public static void main(String[] args) {
         DBI dbi = new DBI("jdbc:hsqldb:hsql://localhost/xdb", "SA", "");
         UrlDAO dao = dbi.onDemand(UrlDAO.class);
 
-        dao.markUrlsAsWip(5);
+        // dao.markUrlsAsWip(5);
 
         List<Url> result = dao.getWipUrls();
         for (Url u : result) {
-            System.out.println(u.getUrl());
+            System.out.println("wget -O " + DOWNLOAD_DIR + "/" + u.getId() + " " + u.getUrl());
         }
     }
 }
