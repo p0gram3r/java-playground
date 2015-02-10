b52f424173ac36334b5037b5d789fe002ebb066b
p0gram3r <p0gram3r@gmail.com>
2015-02-10 20:53:48 +0100

    reformatting code

--- pic-archive/src/main/java/ae/picarchive/PrepareDownloads.java
+++ pic-archive/src/main/java/ae/picarchive/PrepareDownloads.java
@@ -9,9 +9,6 @@ import ae.picarchive.dao.UrlDAO;
 public class PrepareDownloads {
 
     public static void main(String[] args) {
-        DAOFactory daoFactory = new DAOFactory(DB_URL, DB_USER, DB_PASS);
-        UrlDAO dao = daoFactory.getUrlDAO();
-
         int limit = 10;
         if (args.length == 1) {
             try {
@@ -21,6 +18,9 @@ public class PrepareDownloads {
                 // use default
             }
         }
+
+        DAOFactory daoFactory = new DAOFactory(DB_URL, DB_USER, DB_PASS);
+        UrlDAO dao = daoFactory.getUrlDAO();
 
         System.out.println("Preparing " + limit + " urls for download...");
         dao.markUrlsAsWip(limit);
