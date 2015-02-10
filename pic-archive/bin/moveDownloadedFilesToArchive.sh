52ae013c68a7c20ae403850ae37e8a8ae888b4d5
p0gram3r <p0gram3r@gmail.com>
2015-02-10 23:34:35 +0100

    adding registerFile command

--- pic-archive/bin/moveDownloadedFilesToArchive.sh
+++ pic-archive/bin/moveDownloadedFilesToArchive.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $0)/..
-DOWNLOAD_DIR=$BASEDIR/.downloads
-ARCHIVE_DIR=$BASEDIR/.archive
 
+source $BASEDIR/bin/config.sh
 
 TOTAL_FILE_COUNT=$(ls ${DOWNLOAD_DIR}/ | wc -l)
 if [ $TOTAL_FILE_COUNT == 0 ]; then
@@ -20,7 +19,7 @@ for FILE in ${DOWNLOAD_DIR}/*; do
     continue
   fi
 
-  # the MD5 hash determines the target directory
+  # the MD5 hash is the basis fordetermines the target directory
   HASH=($(md5sum $FILE))
 
   # split the hash into 11 parts
@@ -31,12 +30,18 @@ for FILE in ${DOWNLOAD_DIR}/*; do
   done
 
   # create the target directory if necessary
-  PARENT_DIR=$ARCHIVE_DIR/${PARTS[0]}/${PARTS[1]}/${PARTS[2]}/${PARTS[3]}/${PARTS[4]}/${PARTS[5]}/${PARTS[6]}/${PARTS[7]}/${PARTS[8]}/${PARTS[9]}/${PARTS[10]}/
+  PARENT_DIR="${ARCHIVE_DIR}/${PARTS[0]}/${PARTS[1]}/${PARTS[2]}/${PARTS[3]}/${PARTS[4]}/${PARTS[5]}/${PARTS[6]}/${PARTS[7]}/${PARTS[8]}/${PARTS[9]}/${PARTS[10]}"
   mkdir -p $PARENT_DIR
 
-  # move the file
-  mv $FILE $PARENT_DIR
+  # register the file with the url
+  URL_ID=$(basename "$FILE")
+  TARGET_FILE_PATH="${PARENT_DIR}/${HASH}"
+  java -cp $BASEDIR/target/pic-archive.jar ae/picarchive/RegisterFile "$URL_ID" "$TARGET_FILE_PATH"
 
+  # move the file
+  mv "$FILE" "$TARGET_FILE_PATH"
+
+  # status update
   FILE_COUNT=$(expr $FILE_COUNT + 1)
   if [ $(expr $FILE_COUNT % 10) == 0 ]; then
     echo "finished $FILE_COUNT of $TOTAL_FILE_COUNT"
