07f1cb1802200ff5b4e63c6322dac579cef11f39
p0gram3r <p0gram3r@gmail.com>
2015-02-10 22:16:29 +0100

    updating database schema

--- pic-archive/src/main/resources/db/1.sql
+++ pic-archive/src/main/resources/db/1.sql
@@ -1,20 +1,5 @@
-create table urls (
+CREATE TABLE urls (
 	id INTEGER IDENTITY,
-	url varchar(500) NOT NULL UNIQUE,
-	status varchar(10) DEFAULT 'NEW' NOT NULL check (status in ('NEW', 'WIP', 'ERROR', 'DONE'))
+	url VARCHAR(500) NOT NULL UNIQUE,
+	status VARCHAR(10) DEFAULT 'NEW' NOT NULL CHECK (status IN ('NEW', 'WIP', 'DONE'))
 );
-
--- TODO
--create table files (
--    name varchar(35) PRIMARY KEY,
--    path VARCHAR(500) NOT NULL
--);
-
-
---
--- WIP
---
-
-update urls 
-set status='WIP' 
-where id in (select id from urls where status = 'NEW' LIMIT 3)
