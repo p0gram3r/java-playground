4bd72a4d058d8ffb29b4d67576d94992e8dd4f63
p0gram3r <p0gram3r@gmail.com>
2015-02-11 23:41:22 +0100

    updating database name

--- pic-archive/src/main/java/ae/picarchive/Configuration.java
+++ pic-archive/src/main/java/ae/picarchive/Configuration.java
@@ -2,7 +2,7 @@ package ae.picarchive;
 
 public class Configuration {
 
-    public static final String DB_URL = "jdbc:hsqldb:hsql://localhost/xdb";
+    public static final String DB_URL = "jdbc:hsqldb:hsql://localhost/picArchive";
     public static final String DB_USER = "SA";
     public static final String DB_PASS = "";
 
