c4beb904f7042a1c4dc67d0604294f0f309f91a4
p0gram3r <p0gram3r@gmail.com>
2015-02-20 12:35:23 +0100

    introducing buffer to url importer

--- pic-archive/src/main/java/org/p0gram3r/picarchive/dao/UrlDAO.java
+++ pic-archive/src/main/java/org/p0gram3r/picarchive/dao/UrlDAO.java
@@ -6,6 +6,7 @@ import org.p0gram3r.picarchive.entity.Url;
 import org.p0gram3r.picarchive.entity.UrlMapper;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.SqlBatch;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
@@ -14,6 +15,9 @@ public interface UrlDAO {
 
     @SqlUpdate("INSERT INTO urls (url) values (:url)")
     void storeNewUrl(@Bind("url") String url);
+
+    @SqlBatch("INSERT INTO urls (url) values (:url)")
+    void storeListOfNewUrl(@Bind("url") List<String> listOfUrls);
 
     @SqlUpdate("UPDATE urls SET status = 'READY' WHERE id IN (SELECT id FROM urls WHERE status = 'NEW' LIMIT :limit)")
     void markMultipleUrlsAsReady(@Bind("limit") int limit);
