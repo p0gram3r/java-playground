1df393a95c24fb011a872ce3118b404072af02a1
anehrlich <anehrlich@paypal.com>
2015-03-09 17:11:39 +0100

    changing exception handling

--- secure-password-hashes/src/main/java/org/p0gram3r/playground/securepasswordhashes/SecurePasswordHashesMain.java
+++ secure-password-hashes/src/main/java/org/p0gram3r/playground/securepasswordhashes/SecurePasswordHashesMain.java
@@ -3,31 +3,43 @@ package org.p0gram3r.playground.securepasswordhashes;
 import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
+import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
 public class SecurePasswordHashesMain {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         String password = "secretPassword123!";
-        String salt = "salt";
-        int iterations = 1;
-        int keyLength = 19;
+        String salt = "someCrazySalt";
+        int iterations = 1000;
+        int keyLength = 192;
 
         String hash = hashPassword(password, salt, iterations, keyLength);
+
         System.out.println(hash);
     }
 
-    public static String hashPassword(String password, String salt, int iterations, int keyLength)
-            throws NoSuchAlgorithmException, InvalidKeySpecException {
-        char[] passwordChars = password.toCharArray();
-        byte[] saltBytes = salt.getBytes();
+    public static String hashPassword(String password, String salt, int iterations, int keyLength) {
+        try {
+            char[] passwordChars = password.toCharArray();
+            byte[] saltBytes = salt.getBytes();
 
-        PBEKeySpec spec = new PBEKeySpec(passwordChars, saltBytes, iterations, keyLength);
-        SecretKeyFactory key = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        byte[] hashedPassword = key.generateSecret(spec).getEncoded();
-        return String.format("%x", new BigInteger(hashedPassword));
+            KeySpec spec = new PBEKeySpec(passwordChars, saltBytes, iterations, keyLength);
+            SecretKeyFactory key = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            SecretKey secretKey = key.generateSecret(spec);
+
+            byte[] hashedPassword = secretKey.getEncoded();
+            return String.format("%x", new BigInteger(hashedPassword));
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
