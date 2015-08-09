9a0035263b1db26096c99484fc58951f7d6f4c07
p0gram3r <p0gram3r@gmail.com>
2015-08-09 16:09:24 +0200

    solving problem 56

--- projecteuler.net/src/main/java/ProjectEulerMain.java
+++ projecteuler.net/src/main/java/ProjectEulerMain.java
@@ -1,10 +1,10 @@
-import problem.P303_MultiplesWithSmallDigits;
+import problem.P056_PowerfulDigitSum;
 import problem.Problem;
 
 public class ProjectEulerMain {
 
     public static void main(String[] args) {
-        Problem p = new P303_MultiplesWithSmallDigits();
+        Problem p = new P056_PowerfulDigitSum();
 
         System.out.println(p.getSolution());
     }
