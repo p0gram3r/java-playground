2f3d122e3e1ff68dfcde7f54e546e91523d2fb2d
p0gram3r <p0gram3r@gmail.com>
2016-03-20 14:06:24 +0100

    adding more examples

--- gson/src/main/java/org/p0gram3r/playground/Dataset.java
+++ gson/src/main/java/org/p0gram3r/playground/Dataset.java
@@ -1,7 +1,9 @@
 package org.p0gram3r.playground;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.annotations.SerializedName;
 
@@ -11,4 +13,26 @@ public class Dataset {
 
     @SerializedName("album_images")
     List<AlbumImages> images = new ArrayList<AlbumImages>();
+
+    private Map<String, Object> otherProperties = new HashMap<String, Object>();
+
+    public String getAlbum_id() {
+        return album_id;
+    }
+
+    public void setAlbum_id(String album_id) {
+        this.album_id = album_id;
+    }
+
+    public String getAlbum_title() {
+        return album_title;
+    }
+
+    public void setAlbum_title(String album_title) {
+        this.album_title = album_title;
+    }
+
+    public Object get(String name) {
+        return otherProperties.get(name);
+    }
 }
