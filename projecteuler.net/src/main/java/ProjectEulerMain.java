74d0ca0ad4ae471f112026fdbfaa18c330e28859
p0gram3r <p0gram3r@gmail.com>
2016-09-22 21:11:00 +0200

    P019

--- projecteuler.net/src/main/java/ProjectEulerMain.java
+++ projecteuler.net/src/main/java/ProjectEulerMain.java
@@ -1,10 +1,10 @@
-import problem.P056_PowerfulDigitSum;
+import problem.P019_CountingSundays;
 import problem.Problem;
 
 public class ProjectEulerMain {
 
     public static void main(String[] args) {
-        Problem p = new P056_PowerfulDigitSum();
+        Problem p = new P019_CountingSundays();
 
         System.out.println(p.getSolution());
     }
