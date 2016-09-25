82d9500d7f0c0902902659bc037ec0e59085deac
p0gram3r <p0gram3r@gmail.com>
2016-09-25 16:50:44 +0200

    WIP P303

--- projecteuler.net/src/main/java/ProjectEulerMain.java
+++ projecteuler.net/src/main/java/ProjectEulerMain.java
@@ -1,10 +1,10 @@
-import problem.P019_CountingSundays;
+import problem.P303_MultiplesWithSmallDigits;
 import problem.Problem;
 
 public class ProjectEulerMain {
 
     public static void main(String[] args) {
-        Problem p = new P019_CountingSundays();
+        Problem p = new P303_MultiplesWithSmallDigits();
 
         System.out.println(p.getSolution());
     }
--- projecteuler.net/src/main/java/problem/P303_MultiplesWithSmallDigits.java
+++ projecteuler.net/src/main/java/problem/P303_MultiplesWithSmallDigits.java
@@ -1,16 +1,27 @@
 package problem;
 
-import java.math.BigInteger;
+import com.google.common.collect.ImmutableSet;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
- * For a positive integer n, define f(n) as the least positive multiple of n that, written in base 10, uses only digits ≤ 2.
- * 
+ * For a positive integer n, define f(n) as the least positive multiple of n that, written in base 10, uses only
+ * digits ≤ 2.
+ * <p>
  * Thus f(2)=2, f(3)=12, f(7)=21, f(42)=210, f(89)=1121222.
- * 
+ * <p>
  * Also, sum(1,100, f(n)/n) = 11363107
- * 
+ * <p>
  * Find sum(1,10000, f(n)/n)
- * 
  */
 public class P303_MultiplesWithSmallDigits implements Problem {
 
@@ -18,85 +29,95 @@ public class P303_MultiplesWithSmallDigits implements Problem {
 
     @Override
     public Object getSolution() {
-        return sumFunctionQuotients(10);
+
+        try {
+            File file = new File("multiples_of_n.txt");
+
+            Set<String> values = ImmutableSet.of("0", "1", "2");
+            Collection<String> perms = createPermutations(values, values, 1, 12);
+            writeToTempFile(perms, file);
+
+            int nCount = 100;
+
+            Map<Long, Long> mapping = getMapping(file, nCount);
+            if (mapping.size() != nCount) {
+                throw new RuntimeException("mapping does not contain all necessary values!");
+            }
+
+            long result = 0;
+            for (Long m : mapping.values()) {
+                result += m;
+            }
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    /**
-     * Returns the factor x so that f(n) = n * x returns a number only using digits ≤ 2
-     * 
-     * @param n
-     * @return
-     */
-    protected BigInteger getFactorForF(int n) {
-        BigInteger nAsBigInt = new BigInteger(Integer.toString(n));
-        BigInteger factor = null;
+    Map<Long, Long> getMapping(File fileWithMultiples, int nCount) throws FileNotFoundException {
+        Map<Long, Long> mapping = new TreeMap<>();
 
-        for (int i = 1;;) {
-            nAsBigInt = new BigInteger(Integer.toString(n));
-            factor = new BigInteger(Integer.toString(i));
+        Scanner scanner = new Scanner(fileWithMultiples);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
 
-            BigInteger multiple = nAsBigInt.multiply(factor);
-            if (multiple.toString().matches(F_REGEX)) {
+            // avoid useless computation
+            if (mapping.size() == nCount - 2)
+            {
                 break;
             }
 
-            if (n % 10 == 9) {
-                if (i % 10 == 0) {
-                    i += 8;
-                }
-                else {
-                    i += 1;
-                }
+            // ignore illegal values
+            if (!line.matches(F_REGEX)) {
+                System.err.println("line does not match regex: " + line);
+                continue;
             }
-            else {
-                i += 1;
+
+            // 0 is trivial and can be ignored
+            Long multiple = Long.valueOf(line);
+            if (multiple == 0) {
+                continue;
+            }
+
+            for (long n = nCount; n > 2; n--) {
+                long m = multiple / n;
+                if (n * m == multiple) {
+                    Long oldM = mapping.get(n);
+                    if (oldM == null || m < oldM) {
+                        mapping.put(n, m);
+                    }
+                }
             }
         }
 
-        return factor;
+        // add trivial values
+        mapping.put(1L, 1L);
+        mapping.put(2L, 1L);
+
+        return mapping;
     }
 
-    // ///
-    // old and slow implementation
-    // ///
-
-    /**
-     * For a positive integer n, define f(n) as the least positive multiple of n that, written in base 10, uses only digits ≤
-     * 2.
-     * 
-     * @param n
-     * @return
-     */
-    protected BigInteger f(int n) {
-        BigInteger multiple;
-
-        for (int i = 1;; i += 1) {
-            multiple = new BigInteger(Integer.toString(n));
-            multiple = multiple.multiply(new BigInteger(Integer.toString(i)));
-
-            if (multiple.toString().matches(F_REGEX)) {
-                break;
-            }
+    void writeToTempFile(Collection<String> data, File file) throws IOException {
+        PrintWriter w = new PrintWriter(file);
+        for (String p : data) {
+            w.println(p);
         }
-
-        return multiple;
+        w.flush();
     }
 
-    protected BigInteger sumFunctionQuotients(int maxN) {
-        BigInteger result = BigInteger.ZERO;
-
-        for (int n = 1; n <= maxN; n += 1) {
-            // BigInteger functionResult = f(n);
-            // BigInteger nAsBigInt = new BigInteger(Integer.toString(n));
-            // BigInteger quot = functionResult.divide(nAsBigInt);
-            //
-            // result = result.add(quot);
-            // System.out.println("  f: " + functionResult + "  n: " + nAsBigInt + "   = " + quot + " --> " + result);
-
-            result = result.add(getFactorForF(n));
+    Collection<String> createPermutations(Set<String> allPrefixes, Set<String> values, int count, int maxCount) {
+        if (count >= maxCount) {
+            return allPrefixes;
         }
 
-        return result;
+        Set<String> newPrefixes = new LinkedHashSet<>();
+        newPrefixes.addAll(allPrefixes);
+        for (String p : allPrefixes) {
+            for (String v : values) {
+                newPrefixes.add("" + p + v);
+            }
+        }
+        return (createPermutations(newPrefixes, values, count + 1, maxCount));
     }
 
 }
