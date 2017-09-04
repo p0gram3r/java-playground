e74403615365d6707e3bdb57c915cc337b14aa49
André Karstensen <akarstensen@ebay.com>
2017-09-04 17:11:40 +0200

    fixing other projects

---
+++ rest-clients/src/main/java/playground/GitHubRepo.java
@@ -0,0 +1,17 @@
+package playground;
+
+public class GitHubRepo {
+    private int id;
+    private String name;
+
+    public GitHubRepo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
