489b88c08ea32df864ed9c1966437faa6b784e58
Andre Karstensen <akarstensen@ebay.com>
2017-12-12 17:21:52 +0100

    cleaning up

--- dropwizard/src/main/java/playground/dropwizard/HelloWorldApplication.java
+++ dropwizard/src/main/java/playground/dropwizard/HelloWorldApplication.java
@@ -1,6 +1,13 @@
 package playground.dropwizard;
 
+import static com.google.common.collect.Maps.fromProperties;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.text.StrSubstitutor;
+
 import com.google.common.collect.ImmutableMap;
+
 import io.dropwizard.Application;
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
@@ -8,14 +15,9 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
-import org.apache.commons.lang3.text.StrSubstitutor;
 import playground.dropwizard.config.ServiceConfiguration;
 import playground.dropwizard.health.TemplateHealthCheck;
 import playground.dropwizard.resource.HelloResource;
-
-import java.util.Map;
-
-import static com.google.common.collect.Maps.fromProperties;
 
 public class HelloWorldApplication extends Application<ServiceConfiguration> {
 
@@ -24,31 +26,33 @@ public class HelloWorldApplication extends Application<ServiceConfiguration> {
     }
 
     @Override
-    public String getName() {
-        return "hello-world";
-    }
-
-    @Override
     public void initialize(Bootstrap<ServiceConfiguration> bootstrap) {
+        // read System Properties and ENV variables and
         bootstrap.setConfigurationSourceProvider(new SubstitutingSourceProvider(
             new ResourceConfigurationSourceProvider(),
             new StrSubstitutor(resolveEnvironmentProperties())
         ));
 
-        initSwaggerBundle(bootstrap);
+        // add bundle for swagger UI
+        bootstrap.addBundle(new SwaggerBundle<ServiceConfiguration>() {
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(ServiceConfiguration cfg) {
+                return cfg.swaggerBundleConfiguration;
+            }
+        });
     }
 
 
     @Override
     public void run(ServiceConfiguration configuration, Environment environment) {
-        final HelloResource resource = new HelloResource(
+        final HelloResource helloResource = new HelloResource(
             configuration.template,
             configuration.defaultName
         );
-        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.template);
 
-        environment.healthChecks().register("template", healthCheck);
-        environment.jersey().register(resource);
+        environment.jersey().register(helloResource);
+
+        environment.healthChecks().register("template", new TemplateHealthCheck(configuration.template));
     }
 
     private Map<String, String> resolveEnvironmentProperties() {
@@ -58,12 +62,4 @@ public class HelloWorldApplication extends Application<ServiceConfiguration> {
             .build();
     }
 
-    private void initSwaggerBundle(Bootstrap<ServiceConfiguration> bootstrap) {
-        bootstrap.addBundle(new SwaggerBundle<ServiceConfiguration>() {
-            @Override
-            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(ServiceConfiguration cfg) {
-                return cfg.swaggerBundleConfiguration;
-            }
-        });
-    }
 }
